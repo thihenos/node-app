074c87d6c0c5e5ac509e8f240ca49c3db6fcb36e
Thiago Silva <thihenos@gmail.com>
2020-08-23 23:10:56 -0300

    Module 0

--- 1-FirstPages/server/server.js
+++ 1-FirstPages/server/server.js
@@ -1,14 +1,31 @@
-const createServer = require('./create-server')
-const app = createServer.app
-
+const express = require('express')
+const path = require('path')
+const flash = require('connect-flash')
+const cons = require('consolidate')
+const dust = require('dustjs-helpers')
+const dustlinkedin = require('dustjs-linkedin')
+const cookieParser = require('cookie-parser')
+const app = express()
+const server = require('http').createServer(app)
 const port = process.env.PORT || 4000
 
-// Subindo server em porta desejada
-const express_server = app.listen(port, () => {
-	console.log('----------- APP INICIADA -----------')
-})
-/* Fim Chamada das rotas REST da aplicação */
+  // Sinalizando ao Node a view engine
+  app.engine('dust',cons.dust)
 
-module.exports = {
-  express_server
-}
+  /* Configurando a engine que será responsável por compilar o front-end */
+  // Configurando a engine dust para compilar nossos arquivos que estarão na pasta views com extensão .dust
+  app.set('view engine', 'dust','dustjs-linkedin');
+  // Configurando qual pasta será a visualização
+  app.set('views',path.resolve(__dirname , '../views/'));
+  /* Fim de configuração de engine */
+
+  /* Configuração de pastas fixas e estáticas na aplicação */
+  app.use(express.static(path.resolve(__dirname , '../public/')))
+  /* */
+
+  require('../views/routes-views')(app)
+
+  // Subindo server em porta desejada
+  app.listen(port, () => {
+	console.log('----------- APP INICIADA -----------')
+  })
